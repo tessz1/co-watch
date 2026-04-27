220e37e598952a1b4d2966569924705c9ee5c1e5
tessz <tess4184@gmail.com>
2026-04-27 16:23:01 +0300

    add background main, change cmnjs -> module

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const app = express()
-const server = http.createServer(app)
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
+// __dirname для ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// static
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    socket.on('sync-event', (data) => {
-        socket.broadcast.emit('sync-event', data);
-    });
-    socket.on('my-time', (data) => {
-        socket.broadcast.emit('remote-time', data);
-    });
+
     socket.on('join-room', (roomName) => {
         socket.join(roomName);
         socket.currentRoom = roomName;
+
         const clients = io.sockets.adapter.rooms.get(roomName);
         const isLeader = clients ? clients.size === 1 : true;
+
         socket.emit('room-joined', {
-            roomName: roomName,
+            roomName,
             role: isLeader ? 'leader' : 'follower'
         });
     });
-    socket.on('sync-time', (data) => {
-        socket.to(socket.currentRoom).emit('sync-time', data);
-    });
+
     socket.on('sync-event', (data) => {
         socket.to(socket.currentRoom).emit('sync-event', data);
     });
-})
 
+    socket.on('my-time', (data) => {
+        socket.to(socket.currentRoom).emit('remote-time', data);
+    });
 
+    socket.on('sync-time', (data) => {
+        socket.to(socket.currentRoom).emit('sync-time', data);
+    });
+
+});
 
 server.listen(3333, () => {
     console.log('Сервер с WebSocket запущен');
